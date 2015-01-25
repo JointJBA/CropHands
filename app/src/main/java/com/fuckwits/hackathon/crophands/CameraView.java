cb6d1d914c570a7b39d454fe62348c5731d930d0
Belal <belosbelos@live.com>
2015-01-25 12:37:17 -0500

    bunchabullshit

--- app/src/main/java/com/fuckwits/hackathon/crophands/CameraView.java
+++ app/src/main/java/com/fuckwits/hackathon/crophands/CameraView.java
@@ -6,13 +6,14 @@ package com.fuckwits.hackathon.crophands;
 import android.content.Context;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class CameraView extends SurfaceView implements SurfaceHolder.Callback
 {
     private SurfaceHolder surfaceHolder = null;
-    private Camera camera = null;
+    public Camera camera = null;
 
     @SuppressWarnings("deprecation")
     public CameraView(Context context)
@@ -28,18 +29,65 @@ public class CameraView extends SurfaceView implements SurfaceHolder.Callback
      * (non-Javadoc)
      * @see android.view.SurfaceHolder.Callback#surfaceCreated(android.view.SurfaceHolder)
      */
+
+    public static Camera cam(){
+        final String TAG = "cam";
+        int numCameras = Camera.getNumberOfCameras();
+        if(numCameras == 0){
+            Log.w(TAG, "No Cameras");
+            return null;
+        }
+
+        int index = 0;
+        while(index < numCameras){
+            Camera.CameraInfo cameraInfo = new Camera.CameraInfo();
+            Camera.getCameraInfo(index,cameraInfo);
+            if(cameraInfo.facing==Camera.CameraInfo.CAMERA_FACING_BACK){
+                break;
+            }
+            index++;
+        }
+        Camera camera = null;
+        long timeout = System.currentTimeMillis() + 1000;
+        int attempt = 0;
+        while(camera == null && System.currentTimeMillis() < timeout) {
+            attempt++;
+            Log.v(TAG, "Sleeping 100ms - attempt " + attempt);
+            try {
+                Thread.sleep(100);
+            }
+            catch(Exception e) {
+
+            }
+
+            try {
+                if (index < numCameras) {
+                    Log.i(TAG, "Opening camera #" + index);
+                    camera = Camera.open(index);
+                } else {
+                    Log.i(TAG, "No camera facing back; returning camera #0");
+                    camera = Camera.open(0);
+                }
+            } catch (RuntimeException e) {
+                Log.w(TAG, "RuntimeException: " + e.getMessage());
+            }
+        }
+
+        return camera;
+    }
+
     @Override
     public void surfaceCreated(SurfaceHolder holder)
     {
-        /*if (camera != null) {
+        if (camera != null) {
             releaseCamera();
         }
         try {
-            Thread.sleep(1000);
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }*/
-
+        }
+        Log.d("A","Camera has been opened");
         camera = Camera.open();
 
         // Set the Hotfix for Google Glass
@@ -105,5 +153,9 @@ public class CameraView extends SurfaceView implements SurfaceHolder.Callback
             camera.release();
             camera = null;
         }
+    }
+
+    public String takePicture() {
+        return "";
     }
 }
