e5a66cf02d8b3f5a1f9b6b78af9c598ab9607e72
not-cosmo <atillaabc@gmail.com>
2015-01-25 01:03:17 -0500

    Update ApplicationTest.java

--- app/src/androidTest/java/com/fuckwits/hackathon/crophands/ApplicationTest.java
+++ app/src/androidTest/java/com/fuckwits/hackathon/crophands/ApplicationTest.java
@@ -1,10 +1,14 @@
 package com.fuckwits.hackathon.crophands;
+package com.google.android.glass.action.VOICE_TRIGGER;
+
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ * 
+ * public static final VoiceTriggers.Command TAKE_A_PICTURE
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
